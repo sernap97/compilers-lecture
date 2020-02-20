8fc89dcb96f55353fca3ef24218ae69e3da8ef62
sernap97 <sebas.ssp97@gmail.com>
2020-02-19 18:33:38 -0600

    A01633102-homework-01
    Signed-off-by: sernap97 <sebas.ssp97@gmail.com>

--- labs/01/asm-analitics.sh
+++ labs/01/asm-analitics.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/bash
 
-echo 'hi, this is done :)'
+declare -A functions
+declare -A instructions
+regex_func="(^[0-9a-f]+) <(.*)>"
+regex_instr="[[:space:]]*[0-9a-f]+:[[:space:]]+([0-9a-z]+ )+[[:space:]]+([0-9a-z]+)"
 
-echo 'mistake'
+echo 'Hi, this is the output of the analysis:'
+while read line
+do
+    if  [[ $line =~ $regex_func ]]; then
+        functions[${BASH_REMATCH[1]}]=${BASH_REMATCH[2]}
+    elif [[ $line =~ $regex_instr ]]; then
+        instructions[${BASH_REMATCH[2]}]+=1
+    fi
+
+done < "$1"
+
+echo -e "    You have ${#instructions[@]} kinds of instructions in this object file:"
+for i in "${!instructions[@]}"
+do
+    printf "\t%-15s: %10s\n" "$i" "Executed ${#instructions[$i]} times"
+done
+
+echo -e "    You have ${#functions[@]} functions:"
+for i in "${!functions[@]}"
+do
+    printf "\t%-23s: %10s\n" "${functions[$i]}"  "Located at $i addr"
+done
